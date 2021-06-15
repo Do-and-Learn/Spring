a9ffbd3efbd6dd1821b581912c333a20c2ccd559
Teddy Lee <teddy.lee@sunbirddcim.com>
2021-06-15 21:26:23 +0800

    First example

---
+++ src/test/java/idv/teddy/controller/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package idv.teddy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+@WebMvcTest(controllers = HomeController.class)
+class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(xpath("normalize-space()").string("哩賀!"));
+    }
+}
