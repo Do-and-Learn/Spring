fef7b39ebd29521321f9647fd634b471925ea6cd
Teddy Lee <teddy.lee@sunbirddcim.com>
2021-06-15 22:33:10 +0800

    初探 Spring Security

--- src/test/java/idv/teddy/controller/HomeControllerTest.java
+++ src/test/java/idv/teddy/controller/HomeControllerTest.java
@@ -3,12 +3,14 @@ package idv.teddy.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 @WebMvcTest(controllers = HomeController.class)
+@WithMockUser
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
