ceab5780b38ee7a0e17b22c41e915d350ae6b180
Teddy Lee <teddy.lee@sunbirddcim.com>
2021-06-16 22:36:44 +0800

    add unauthenticated

--- src/test/java/idv/teddy/controller/HomeControllerTest.java
+++ src/test/java/idv/teddy/controller/HomeControllerTest.java
@@ -3,21 +3,30 @@ package idv.teddy.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
 @WebMvcTest(controllers = HomeController.class)
-@WithMockUser
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testHello() throws Exception {
+    @WithMockUser
+    public void testHelloWithValidUser() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(xpath("normalize-space()").string("哩賀!"));
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testHelloWithInvalidUser() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(unauthenticated());
     }
 }
