17f3acca40be149375a707ac08018d3fe9eaf265
Teddy Lee <teddy.lee@sunbirddcim.com>
2021-06-16 22:55:15 +0800

    In-Memory Authentication

--- src/main/java/idv/teddy/SpringApplication.java
+++ src/main/java/idv/teddy/SpringApplication.java
@@ -1,12 +1,36 @@
 package idv.teddy;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class SpringApplication {
 
-	public static void main(String[] args) {
-		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        User.UserBuilder users = User.builder().passwordEncoder(passwordEncoder::encode);
+        UserDetails user = users
+                .username("user")
+                .password("blabla")
+                .roles("USER")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("secret")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
 }
