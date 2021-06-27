ede146169bdb9c0e7122872d79b97739ffaf7bc0
Teddy Lee <teddy12090@gmail.com>
2021-06-27 19:23:44 +0800

    Authentication

--- src/main/java/idv/teddy/controller/HomeController.java
+++ src/main/java/idv/teddy/controller/HomeController.java
@@ -1,10 +1,13 @@
 package idv.teddy.controller;
 
+import idv.teddy.domain.User;
 import idv.teddy.payload.RegisterForm;
 import idv.teddy.repository.UserDetailsRepository;
 import lombok.Data;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +21,9 @@ public class HomeController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/")
-    private String hello() {
+    private String hello(Authentication authentication, Model model) {
+        User user = (User) authentication.getPrincipal();
+        model.addAttribute("name", user.getUsername());
         return "hello";
     }
 
