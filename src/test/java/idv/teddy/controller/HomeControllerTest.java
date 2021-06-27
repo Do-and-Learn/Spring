e78194dd53174421e2272395ffe979840ee234d2
Teddy Lee <teddy12090@gmail.com>
2021-06-27 18:26:41 +0800

    fix unit test

--- src/test/java/idv/teddy/controller/HomeControllerTest.java
+++ src/test/java/idv/teddy/controller/HomeControllerTest.java
@@ -2,7 +2,8 @@ package idv.teddy.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,7 +12,8 @@ import static org.springframework.security.test.web.servlet.response.SecurityMoc
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
-@WebMvcTest(controllers = HomeController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
