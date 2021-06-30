c53de391cb3adebe9343b6d021d1858cf0976136
Teddy Lee <teddy.lee@sunbirddcim.com>
2021-06-30 22:23:01 +0800

    Spring entity

---
+++ src/main/java/idv/teddy/domain/Item.java
@@ -0,0 +1,22 @@
+package idv.teddy.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Item (name, price) VALUES ('iPhone 12', 26900);
+INSERT INTO Item (name, price) VALUES ('iPhone 12 mini', 23900);
